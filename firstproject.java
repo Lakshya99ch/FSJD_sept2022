2e82ec4d1059eb640093c445734477e0a4d28b9c
Lakshya Chaudhary <lakshyachaudhary0098@gmail.com>
2022-09-22 20:10:32 +0530

    First Commit

---
+++ firstproject.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class firstproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first Java project");
+
+	}
+
+}
